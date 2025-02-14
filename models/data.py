1ad2ba9d38321b5cdd587b2308882b19f753a45c
rogui7909 <rogui7909@cyclone.uib.no>
2025-02-14 13:33:33 +0100

    Split files for better readability

--- models/data.py
+++ models/data.py
@@ -1,56 +1,54 @@
 import xarray as xr
 import numpy as np
 import torch
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, TensorDataset, random_split
 
 DATA_DIR = '/Data/gfi/users/rogui7909/code/CESM2_rain_prediction'
-def load_output_data(region=14):
-    ds_rain = xr.open_dataarray(f'{DATA_DIR}/cesm2_amip_pr.nc')*86400
-    ds_mask = xr.open_dataset('/Data/gfi/users/jodor4442/rainfall_regions/K39_ERA5land_lat_weighted_2d_direct.nc')
-    norge_mask = (ds_mask.tp==region).sel(longitude=slice(0,None)).rename(longitude='lon', latitude='lat')*1
-    norge_mask = norge_mask.interp(lon=ds_rain.lon, lat=ds_rain.lat, method='nearest').fillna(0)    
-    ds_out_rain = ds_rain.where(norge_mask==1).mean(['lon','lat'])
-    return ds_out_rain, norge_mask
 
-def get_rain_bool(ds_out_rain, thresh_mm = 1):
-    ds_out_rain = ds_out_rain.where(ds_out_rain>thresh_mm, 0)
-    ds_out_rain = ds_out_rain.where(ds_out_rain<thresh_mm, 1)
-    return ds_out_rain
-    
-def load_input_data(normalized = True):
-    ds_in1 = xr.open_dataarray(f'{DATA_DIR}/cesm2_amip_u850_NH.nc')#.sel(time=ds_out.time)
-    ds_in2 = xr.open_dataarray(f'{DATA_DIR}/cesm2_amip_v850_NH.nc')#.sel(time=ds_out.time)
-    ds_in3 = xr.open_dataarray(f'{DATA_DIR}/cesm2_amip_z500_NH.nc')#.sel(time=ds_out.time)
-    if normalized:
-        ds_in1 = ((ds_in1-ds_in1.mean())/ds_in1.std()).fillna(0)
-        ds_in2 = ((ds_in2-ds_in2.mean())/ds_in2.std()).fillna(0)
-        ds_in3 = ((ds_in3-ds_in3.mean())/ds_in3.std()).fillna(0)
-    ds_in = xr.concat([ds_in1.assign_coords(var_name='u850'), 
-                       ds_in2.assign_coords(var_name='v850'), 
-                       ds_in3.assign_coords(var_name='z500')], dim='var_name')
-    return ds_in
+def get_input_data(input_variable, region_predicted, type_prediction, batch_size):
+    variable_order = dict(z500=0,u850=1,v850=2,tcwv=3,pr=4)
+    input_variable_index = [variable_order[variable] for variable in input_variable]
+
+    data = np.load( "/Data/gfi/users/rogui7909/data/ERA5/numpy_era5_in.npy")
+    data_in = data[:,input_variable_index]
+    tensor_in = torch.Tensor(data_in).type(torch.float32)
+    # tensor_in = torch.load("/Data/gfi/users/rogui7909/data/ERA5/tensor_era5_in.pt", weights_only=True)
 
 
-def xarray_to_dataloaders(ds_in, ds_out, batch_size, split=0.8, shuffle=True):
-    """
-    split in fraction betwwen 0 and 1
-    """
-    indices = np.arange(ds_out.size)
-    if shuffle:
-        np.random.shuffle(indices)
-    split = int(split*ds_out.size)
-    indices_train, indices_test = indices[:split], indices[split:]
-    data_in = ds_in.values#.transpose(1,0,2,3)
-    data_out = ds_out.values
+    with xr.open_dataarray('/Data/gfi/users/rogui7909/era5_rain_norge/ERA5land_daily_TP_indices.nc', chunks = dict(longitude=20, latitude=20)) as ds_out:
+        data_out = ds_out.sel(mask_id=region_predicted)
+        if type_prediction=='quantiles':
+            data_out = (data_out.rank('time', pct=True)//.1).astype(int)
+        data_out = data_out.values
+
+    tensor_out = torch.Tensor(data_out).type(torch.float32)
+    if type_prediction=='quantiles':
+        tensor_out = tensor_out.type(torch.long)
+    elif type_prediction=='regression':
+        tensor_out = tensor_out.reshape(-1,1)
+
+    dataset = TensorDataset(tensor_in, tensor_out)
+
+    # use 20% of data for test
+    trainvalid_set_size = int(len(dataset) * 0.8)
+    test_set_size = len(dataset) - trainvalid_set_size
+    # split the train set into two
+    seed = torch.Generator().manual_seed(42)
+    trainvalid_set, test_set = random_split(dataset, [trainvalid_set_size, test_set_size], generator=seed)
+
+    # use 20% of training data for validation
+    train_set_size = int(len(trainvalid_set) * 0.8)
+    valid_set_size = len(trainvalid_set) - train_set_size
+
+    # split the train set into two
+    seed = torch.Generator().manual_seed(43)
+    train_set, valid_set = random_split(trainvalid_set, [train_set_size, valid_set_size], generator=seed)
 
 
-    X_train = torch.tensor(data_in[indices_train]).type(torch.float32)
-    X_test = torch.tensor(data_in[indices_test]).type(torch.float32)
-    y_train = torch.tensor(data_out[indices_train]).type(torch.LongTensor)
-    y_test = torch.tensor(data_out[indices_test]).type(torch.LongTensor)
+    train_loader = DataLoader(train_set, batch_size=batch_size)
+    valid_loader = DataLoader(valid_set, batch_size=batch_size)
+    test_loader = DataLoader(test_set, batch_size=batch_size)
 
-    train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(TensorDataset(X_test, y_test), batch_size=batch_size, shuffle=True)
+    return train_loader, valid_loader, test_loader
 
-    return train_loader, test_loader, indices_train, indices_test
-
+print('Data ready')
--- rain_lighting.py
+++ rain_lighting.py
@@ -13,6 +13,7 @@ from models.lightning import LitCNN
 from models.losses import DistribLoss
 import random
 import matplotlib
+from models.data import get_input_data
 
 
 matplotlib.use('tkagg')
@@ -34,7 +35,7 @@ learning_rate = 0.001
 batch_size = 128
 num_epochs = 10
 device = 'cuda:1'
-input_variable = ['pr']
+input_variable = ['u850']
 groups = 3
 
 region_predicted = 14
@@ -51,27 +52,13 @@ elif type_prediction == 'regression':
     num_classes =  1
 
 
-variable_order = dict(z500=0,
-                      u850=1,
-                      v850=2,
-                      tcwv=3,
-                      pr=4)
-input_variable_index = [variable_order[variable] for variable in input_variable]
-
-
-process_variables = dict(z500='standard',
-                         u850='std_only',
-                         v850='std_only',
-                         tcwv='standard',
-                         pr='standard'
-                            )
 config = {
     "hidden_layer_sizes": [32, 64],
     "kernel_sizes": [3],
     "activation": "ReLU",
     "pool_sizes": [2],
     "dropout": 0.5,
-    "batch_size": 256,
+    "batch_size": batch_size,
     "num_epochs": num_epochs,
     "num_classes": num_classes,
     "learning_rate":learning_rate,
@@ -85,85 +72,12 @@ config = {
 
 wandb_logger = WandbLogger(project="Predict-rain-WNorway", config=config, name=f"CNN-{type_prediction} {'-'.join(input_variable)}")
 
-
-lon_extent = slice(-90,89.5)
-lat_extent = slice(20,90)
-
 print('Loading data ...')
 
-# all_data_in = []
-# for variable in config['input_variable']:
-#     with xr.open_dataset(f'/Data/gfi/users/rogui7909/data/ERA5/{variable}.nc') as ds:
-#         data_ = ds.sel(latitude=lat_extent, longitude=lon_extent)[variable].values
-#         time = ds.time
-#         if process_variables[variable] == 'standard':
-#             data_ = (data_ - ds[f'mean_{variable}'].values)/ds[f'std_{variable}'].values
-#         elif process_variables[variable] == 'std_only':
-#             data_ = (data_)/ds[f'std_{variable}'].values
-#         all_data_in.append(data_)
-#         print('   --> ',variable,'loaded')
-# data_in = np.array(all_data_in)
-# data_in = data_in.transpose(1,0,2,3)
-# print('Input data ready')
-
-data = np.load( "/Data/gfi/users/rogui7909/data/ERA5/numpy_era5_in.npy")
-data_in = data[:,input_variable_index]
-tensor_in = torch.Tensor(data_in).type(torch.float32)
-# tensor_in = torch.load("/Data/gfi/users/rogui7909/data/ERA5/tensor_era5_in.pt", weights_only=True)
-
-
-with xr.open_dataarray('/Data/gfi/users/rogui7909/era5_rain_norge/ERA5land_daily_TP_indices.nc', chunks = dict(longitude=20, latitude=20)) as ds_out:
-    data_out = ds_out.sel(mask_id=region_predicted)
-    if type_prediction=='quantiles':
-        data_out = (data_out.rank('time', pct=True)//.1).astype(int)
-    data_out = data_out.values
-
-tensor_out = torch.Tensor(data_out).type(torch.float32)
-if type_prediction=='quantiles':
-    tensor_out = tensor_out.type(torch.long)
-elif type_prediction=='regression':
-    tensor_out = tensor_out.reshape(-1,1)
-
-dataset = TensorDataset(tensor_in, tensor_out)
-
-# use 20% of data for test
-trainvalid_set_size = int(len(dataset) * 0.8)
-test_set_size = len(dataset) - trainvalid_set_size
-# split the train set into two
-seed = torch.Generator().manual_seed(42)
-trainvalid_set, test_set = random_split(dataset, [trainvalid_set_size, test_set_size], generator=seed)
-
-# use 20% of training data for validation
-train_set_size = int(len(trainvalid_set) * 0.8)
-valid_set_size = len(trainvalid_set) - train_set_size
-
-# split the train set into two
-seed = torch.Generator().manual_seed(43)
-train_set, valid_set = random_split(trainvalid_set, [train_set_size, valid_set_size], generator=seed)
-
-
-train_loader = DataLoader(train_set, batch_size=batch_size)
-valid_loader = DataLoader(valid_set, batch_size=batch_size)
-test_loader = DataLoader(test_set, batch_size=batch_size)
-
+train_loader, valid_loader, test_loader = get_input_data(config['input_variable'], config['region_predicted'], config['type_prediction'], config['batch_size'])
+ 
 print('Data ready')
 
-
-# Define lightning class
-
-
-# print('Imports done')
-
-
-
-
-        # df_ =  pd.DataFrame(dict(truth=pl_module.test_step_y, pred=pl_module.test_step_pred))
-
-        # fig, ax = plt.subplots(figsize=(6,6))
-        # sns.heatmap(df_.groupby(['truth','pred']).count().unstack(),
-        #             square=True, cbar=False, annot=True, fmt = '.0f', ax=ax)
-        # wandb.log({ 'confusion_matrix' : wandb.Image(fig) })
-        
 
 trainer = L.Trainer(limit_train_batches=100, max_epochs=num_epochs, logger=wandb_logger, 
                     log_every_n_steps=1, default_root_dir="lightning_checkpoints/", devices=1,
