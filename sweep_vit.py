c56744bb17f9b9196cfee8423f2c40219afe8098
rguilcas <r.guilcas@outlook.com>
2025-04-30 16:20:21 +0200

    ViT sweep

--- sweep_vit.py
+++ sweep_vit.py
@@ -4,27 +4,26 @@ import wandb
 def main():
     wandb.login()
 
-    from predict_extreme_event import main
+    from predict_extreme_event_vit import main
 
     sweep_configuration = {
+        "name": "ViT sweep",
         "method": "bayes",
         "metric": {"goal": "maximize", "name": "diagnostics/auc_pr"},
         "parameters": {
-            "learning_rate": {"max": 1e-2,'min':1e-4, 'distribution':'log_uniform_values'},
-            "inputs" :{"values":[#'u850 v850',
-                                 #'u850 v850 z500',
-                                 'u850 v850 w850',
+            "learning_rate": {"max": 1e-1,'min':1e-5, 'distribution':'log_uniform_values'},
+            "inputs" :{"values":['u850 v850 w850',
                                  'u850 v850 w850 tcwv',
-                                #  'v850 z500 w850',
                                 ]},
-            "conv1_kernel_number":{"values":[12,24]},
-            "num_conv_layer":{"values":[2,3,4]},
-            "size_conv_kernel":{"values":[3,5,7,9]},
-            "spatial_attention":{"values":[True, False]}
+            'patch_size':{"values":[8,16,32]},            # must divide image_size exactly
+            'embedding_dimension':{"values":[128,256,512,1024]},          # embedding dimension
+            'depth_transformer':{"values":[4,5,6,7,8,9,10]},          # number of transformer blocks
+            'attention_heads':{"values":[4,8,16,32]},            # number of attention heads
+            'mlp_dim':{"values":[128,256,512,1024]},                  # feedforward layer size
+            'dropout':{"min": 0., "max": 0.5},
+            'emb_dropout':{"min": 0., "max": 0.5}
         },
     }
-
-
 
     sweep_id = wandb.sweep(sweep=sweep_configuration, project="Predict-rain-WNorway_test")
     wandb.agent(sweep_id, function=main, count=100)
